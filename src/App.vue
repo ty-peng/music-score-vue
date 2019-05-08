fdaa46d5c2358087542b1a9b361018783ec5ae13
ty-peng <pty.me@qq.com>
2019-05-08 13:10:49 +0800

    modify: edit some files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <NavHeader></NavHeader>
+    <!-- <keep-alive> -->
     <router-view></router-view>
+    <!-- </keep-alive> -->
     <BackTop></BackTop>
   </div>
 </template>
